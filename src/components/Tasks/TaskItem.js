52d7f62ed891f440d39313a8f0e5c3070ef3a871
RubensCordeiro <rubensccordeiro@outlook.com>
2022-06-06 15:17:24 -0300

    A few minor tweaks

--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -1,7 +1,7 @@
 function TaskItem(props) {
   const { detailed } = props;
   return (
-    <div className="w-full md:w-3/4 px-4 py-4 bg-zinc-50 border border-zinc-300 rounded cursor-pointer hover:shadow-lg transition duration-200 linear">
+    <div className="w-full md:w-3/4 px-4 py-4 bg-zinc-50 border border-zinc-300 rounded cursor-pointer hover:shadow-lg transition duration-200 linear mb-4">
       <div className="flex justify-between content-center items-center">
         <p className="font-bold">Task Title</p>
         <p className="text-sm">Due: 01/01/2023</p>
@@ -15,13 +15,13 @@ function TaskItem(props) {
       </div>
       <div className="flex content-center items-center justify-center gap-x-8 border-y border-zinc-500">
         <ul className="flex gap-x-8 items-center content-center justify-center py-2 w-full text-xs">
-          <li className="px-2 py-1 rounded-sm border border-zinc-700">
+          <li className="px-2 py-1 rounded-sm border border-zinc-700 text-center">
             Low urgency
           </li>
-          <li className="px-2 py-1 rounded-sm border border-zinc-700">
+          <li className="px-2 py-1 rounded-sm border border-zinc-700 text-center">
             In Progress
           </li>
-          <li className="px-2 py-1 rounded-sm border border-zinc-700">
+          <li className="px-2 py-1 rounded-sm border border-zinc-700 text-center">
             On time
           </li>
         </ul>
--- src/components/Tasks/TaskList.js
+++ src/components/Tasks/TaskList.js
@@ -2,7 +2,10 @@ import TaskItem from "./TaskItem";
 
 function TaskList() {
   return (
-    <div className="mt-2 p-4 bg-zinc-100 rounded-sm overflow-y-auto mx-auto flex flex-col gap-y-8 items-center content-center">
+    <div className="mt-2 px-4 py-2 bg-zinc-100 rounded-sm overflow-y-auto mx-auto flex flex-col items-center content-center">
+      <button className="py-2 px-4 bg-teal-700 rounded text-white border border-transparent-700 mb-2 hover:bg-zinc-50 hover:text-teal-700 hover:border hover:border-teal-700 transition linear duration-150">
+        <p className="text-sm">New Task</p>
+      </button>
       <TaskItem detailed={false} />
       <TaskItem detailed={false} />
       <TaskItem detailed={false} />
