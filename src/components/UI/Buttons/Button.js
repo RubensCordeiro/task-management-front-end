a32083f471dac78de7e63286215be609117f5576
rubens cordeiro <rubensccordeiro@outlook.com>
2022-07-08 15:32:58 -0300

    Workaround on tailwind append bug

--- src/components/UI/Buttons/Button.js
+++ src/components/UI/Buttons/Button.js
@@ -1,12 +1,25 @@
 export default function Button(props) {
-  const { children, type, color, onClick } = props;
-  return (
-    <button
-      className={`py-2 px-4 bg-${color}-800 rounded text-white border border-transparent-700 hover:bg-zinc-50 hover:text-${color}-700 hover:border hover:border-${color}-700 transition linear duration-150 w-32 mx-auto`}
-      type={type}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+  const { children, type, onClick } = props;
+
+  if (type === 'confirm') {
+    return (
+      <button
+        className={`py-2 px-4 bg-red-800 rounded text-white border border-transparent-700 hover:bg-zinc-50 hover:text-red-700 hover:border hover:border-red-700 transition linear duration-150 w-32 mx-auto`}
+        type={type}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  } else if (type === 'cancel') {
+    return (
+      <button
+        className={`py-2 px-4 bg-teal-800 rounded text-white border border-transparent-700 hover:bg-zinc-50 hover:text-teal-700 hover:border hover:border-teal-700 transition linear duration-150 w-32 mx-auto`}
+        type={type}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
 }
--- src/components/UI/Dialog/Dialog.js
+++ src/components/UI/Dialog/Dialog.js
@@ -8,10 +8,10 @@ export default function Dialog(props) {
       <div className="fixed top-0 left-1/2 mt-16 transform -translate-x-1/2 bg-zinc-100 pt-2 pb-2 px-8 rounded-sm shadow flex flex-col justify-center content-center items-center gap-y-2 self-start z-50">
         <p className="font-bold text-lg text-red-800">Are you sure?</p>
         <div className="flex gap-x-2">
-          <Button color="red" onClick={onConfirm}>
+          <Button color="red" onClick={onConfirm} type='confirm'>
             {confirmLabel}
           </Button>
-          <Button color="teal" onClick={onCancel}>
+          <Button color="teal" onClick={onCancel} type='cancel'>
             {cancelLabel}
           </Button>
         </div>
